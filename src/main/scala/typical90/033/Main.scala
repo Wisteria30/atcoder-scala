a3db7e4df4958fe4927e85ba250d2da12b9ea7ff
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-08 14:45:42 +0900

    典型90問 4日目まで

--- src/main/scala/typical90/033/Main.scala
+++ src/main/scala/typical90/033/Main.scala
@@ -3,5 +3,11 @@ import java.util.Scanner
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
 
-  println("Hello World!")
+  val h, w = sc.nextInt
+  if (h == 1 || w == 1) {
+    println(h * w)
+  } else {
+    val ans = ((h + 1) / 2) * ((w + 1) / 2)
+    println(ans)
+  }
 }
