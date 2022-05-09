06946bafd3389bad73f9fc2906907d23aa81f5d5
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-10 02:43:49 +0900

    典型90問 61, 67

--- src/main/scala/typical90/061/Main.scala
+++ src/main/scala/typical90/061/Main.scala
@@ -1,7 +1,19 @@
+import java.io.PrintWriter
 import java.util.Scanner
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
+  val pw = new PrintWriter(System.out)
 
-  println("Hello World!")
+  val q = sc.nextInt
+  var deck = Vector[Int]()
+  for (i <- 0 until q) {
+    val t, x = sc.nextInt
+    t match {
+      case 1 => deck +:= x
+      case 2 => deck :+= x
+      case 3 => pw.println(deck(x - 1))
+    }
+  }
+  pw.flush()
 }
--- src/main/scala/typical90/067/Main.scala
+++ src/main/scala/typical90/067/Main.scala
@@ -1,7 +1,44 @@
-import java.util.Scanner
+import java.io.{BufferedReader, InputStream, InputStreamReader, PrintWriter}
+import java.util.{Scanner, StringTokenizer}
+
+class InputReader(stream: InputStream) {
+  private val reader = new BufferedReader(new InputStreamReader(stream))
+  private var tokenizer: StringTokenizer = new StringTokenizer(reader.readLine())
+
+  def next(): String = {
+    while (!tokenizer.hasMoreTokens) {
+      tokenizer = new StringTokenizer(reader.readLine())
+    }
+    tokenizer.nextToken()
+  }
+}
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
+  val in = new InputReader(System.in)
+  val pw = new PrintWriter(System.out)
+  val n, k = in.next()
+  var ans = n
+  for (_ <- 1 to k.toInt) {
+    ans = convertBase8to9(ans)
+  }
+  pw.println(ans)
+  pw.flush()
 
-  println("Hello World!")
+  def convertBase8to9(n: String): String = {
+    var base10 = 0L
+    for (i <- n.reverse.zipWithIndex) {
+      i match {
+        case (c_num, exp) => base10 += c_num.asDigit * math.pow(8, exp).toLong
+      }
+    }
+
+    var base9 = ""
+    while (base10 > 0) {
+      base9 += base10 % 9
+      base10 /= 9
+    }
+    if (base9.isEmpty) "0"
+    else base9.replace('8', '5').reverse.toLong.toString
+  }
 }
