9eee668187f2cdde254a5b1e2d8b684376369e4e
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-11 00:52:23 +0900

    典型90問 78

--- src/main/scala/typical90/078/Main.scala
+++ src/main/scala/typical90/078/Main.scala
@@ -3,5 +3,21 @@ import java.util.Scanner
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
 
-  println("Hello World!")
+  val n, m = sc.nextInt
+  val graph = Array.fill(n)(IndexedSeq[Int]())
+  for (_ <- 1 to m) {
+    var a, b = sc.nextInt
+    a -= 1
+    b -= 1
+    if (b < a) graph(a) :+= b
+    if (a < b) graph(b) :+= a
+  }
+
+  val ans = graph.foldLeft(0)(
+    (acc, row) => {
+      if (row.size == 1) acc + 1
+      else acc
+    }
+  )
+  println(ans)
 }
