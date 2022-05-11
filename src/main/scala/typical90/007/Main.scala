5b28284d50f0dd0ef7299e3a6d6429177eb7b82a
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-12 03:09:33 +0900

    典型90問 007 ★3

--- src/main/scala/typical90/007/Main.scala
+++ src/main/scala/typical90/007/Main.scala
@@ -3,5 +3,32 @@ import java.util.Scanner
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
 
-  println("Hello World!")
+  val n = sc.nextInt
+  val a = IndexedSeq.fill(n)(sc.nextInt).sorted
+  val q = sc.nextInt
+
+  (0 until q).foreach { _ =>
+    val b = sc.nextInt
+    val idx = meguruBisect(a.size, 0, b)
+    val ans = math.min(math.abs(a(idx) - b), math.abs(a(math.min(idx + 1, a.size - 1)) - b))
+    println(ans)
+  }
+
+  def meguruBisect(ng: Int, ok: Int, tgt: Int): Int = {
+    var ok_ = ok
+    var ng_ = ng
+    while (math.abs(ok_ - ng_) > 1) {
+      val mid = (ok_ + ng_) / 2
+      if (isOK(tgt, mid)) {
+        ok_ = mid
+      } else {
+        ng_ = mid
+      }
+    }
+    ok_
+  }
+
+  def isOK(tgt: Int, x: Int): Boolean = {
+    a(x) <= tgt
+  }
 }
