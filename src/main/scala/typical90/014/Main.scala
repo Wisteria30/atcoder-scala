add4c8e07ad3dc7e74f53c6bf93508a99058e094
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-13 02:19:30 +0900

    典型90問 014

--- src/main/scala/typical90/014/Main.scala
+++ src/main/scala/typical90/014/Main.scala
@@ -3,5 +3,10 @@ import java.util.Scanner
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
 
-  println("Hello World!")
+  val n = sc.nextInt
+  val a = IndexedSeq.fill(n)(sc.nextLong).sorted
+  val b = IndexedSeq.fill(n)(sc.nextLong).sorted
+
+  val ans = a.zip(b).map { case (a, b) => math.abs(a - b) }.sum
+  println(ans)
 }
