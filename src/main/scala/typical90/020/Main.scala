de343212edf218aea2f6742f11f1cc416e59ab8b
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-16 02:55:02 +0900

    典型90問 020

--- src/main/scala/typical90/020/Main.scala
+++ src/main/scala/typical90/020/Main.scala
@@ -2,6 +2,14 @@ import java.util.Scanner
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
+  val INF = 1000000007
 
-  println("Hello World!")
+  val a, b, c = sc.nextLong
+
+  val bc = (0 until b.toInt).foldLeft(1L)((acc, _) => acc * c)
+  if (a < bc) {
+    println("Yes")
+  } else {
+    println("No")
+  }
 }
