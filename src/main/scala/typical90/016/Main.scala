3c26f1497948a0465de93cfb1d38f0a608614659
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-15 15:38:19 +0900

    典型90問 018

--- src/main/scala/typical90/016/Main.scala
+++ src/main/scala/typical90/016/Main.scala
@@ -1,7 +1,27 @@
+import java.io.PrintWriter
 import java.util.Scanner
+import scala.math._
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
+  val pw = new PrintWriter(System.out)
+  val INF = 1000000007
 
-  println("Hello World!")
+  val n = sc.nextLong
+  val a, b, c = sc.nextLong
+  val maxNumberCoin = 9999
+  var ans = INF.toLong
+
+  for {
+    i <- 0 to maxNumberCoin
+    j <- 0 to maxNumberCoin - i
+  } {
+    val last = n - a * i - b * j
+    val cnt = i + j + last / c
+    if (last % c == 0 && last >= 0 && cnt <= maxNumberCoin) {
+      ans = min(ans, cnt)
+    }
+  }
+  pw.println(ans)
+  pw.flush()
 }
--- src/main/scala/typical90/018/Main.scala
+++ src/main/scala/typical90/018/Main.scala
@@ -1,7 +1,33 @@
 import java.util.Scanner
 
+case class Point(x: Double, y: Double, z: Double) {
+  def distance: Double = {
+    math.sqrt(x * x + y * y + z * z)
+  }
+}
+
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
 
-  println("Hello World!")
+  val T = sc.nextDouble
+  val L, x, y = sc.nextDouble
+  val p_xyz = Point(x, y, 0)
+  val q = sc.nextInt
+  for (_ <- 0 until q) {
+    val e = sc.nextDouble
+    println("%.7f".format(solve(e)))
+  }
+
+  def solve(now: Double): Double = {
+    val rDeg = now / T * 360
+    val r_rad = math.toRadians(rDeg)
+    val s_xyz = Point(
+      0,
+      -L / 2 * math.sin(r_rad),
+      (L / 2) - (L / 2 * math.cos(r_rad))
+    )
+    val A = math.sqrt(math.pow(p_xyz.x, 2) + math.pow(s_xyz.y - p_xyz.y, 2))
+    val B = s_xyz.z
+    math.atan2(B, A).toDegrees
+  }
 }
