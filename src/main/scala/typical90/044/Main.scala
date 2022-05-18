c65f67bca811a3fa1b8365b70d896d60fef3872a
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-19 07:43:14 +0900

    典型90問 044

--- src/main/scala/typical90/044/Main.scala
+++ src/main/scala/typical90/044/Main.scala
@@ -1,7 +1,27 @@
 import java.util.Scanner
+import scala.collection.mutable
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
 
-  println("Hello World!")
+  val n, q = sc.nextInt
+  val A = mutable.IndexedSeq.fill(n)(sc.nextInt)
+  val MOD = A.size
+  var rotationCount = 0
+  for (_ <- 0 until q) {
+    val t, x, y = sc.nextInt
+    t match {
+      case 1 =>
+        val modX = ((x - 1) + rotationCount) % MOD
+        val modY = ((y - 1) + rotationCount) % MOD
+        val tmp = A(modX)
+        A(modX) = A(modY)
+        A(modY) = tmp
+      case 2 =>
+        rotationCount = (rotationCount + MOD - 1) % MOD
+      case 3 =>
+        val modX = ((x - 1) + rotationCount) % MOD
+        println(A(modX))
+    }
+  }
 }
