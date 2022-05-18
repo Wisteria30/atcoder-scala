2521bb167b58f84b63b6cd065b61a55251ce5d25
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-18 15:29:20 +0900

    典型90問 038

--- src/main/scala/typical90/038/Main.scala
+++ src/main/scala/typical90/038/Main.scala
@@ -1,7 +1,15 @@
 import java.util.Scanner
+import scala.annotation.tailrec
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
+  val BORDER = 1000000000000000000L
+  val a, b = BigInt(sc.nextLong)
 
-  println("Hello World!")
+  val lcm = a * b / gcd(a, b)
+  if (lcm > BORDER) println("Large")
+  else println(lcm)
+
+  @tailrec
+  def gcd(x: BigInt, y: BigInt): BigInt = if (y == 0) x else gcd(y, x % y)
 }
