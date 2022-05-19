dccc58c007b948701313055b175f0e4d761efc5d
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-20 03:02:32 +0900

    典型90問 046

--- src/main/scala/typical90/046/Main.scala
+++ src/main/scala/typical90/046/Main.scala
@@ -3,5 +3,16 @@ import java.util.Scanner
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
 
-  println("Hello World!")
+  val mod = 46
+  val n: Int = sc.nextInt
+  val A = IndexedSeq.fill(n)(sc.nextInt % mod).groupMapReduce(identity)(_ => 1)(_ + _)
+  val B = IndexedSeq.fill(n)(sc.nextInt % mod).groupMapReduce(identity)(_ => 1)(_ + _)
+  val C = IndexedSeq.fill(n)(sc.nextInt % mod).groupMapReduce(identity)(_ => 1)(_ + _)
+  var ans = for {
+    a <- A
+    b <- B
+    c <- C
+    if (a._1 + b._1 + c._1) % mod == 0
+  } yield BigInt(a._2) * b._2 * c._2
+  println(ans.toSeq.sum)
 }
