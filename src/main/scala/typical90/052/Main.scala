2e7d7f3e57e0296ef09982bfc70a82e2c0a1b15a
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-23 04:04:51 +0900

    典型90問 052

--- src/main/scala/typical90/052/Main.scala
+++ src/main/scala/typical90/052/Main.scala
@@ -2,6 +2,14 @@ import java.util.Scanner
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
+  val MOD = 1000000007
 
-  println("Hello World!")
+  val N = sc.nextInt
+  val A = IndexedSeq.fill(N, 6)(sc.nextInt)
+  var ans = BigInt(1)
+  for (i <- 0 until N) {
+    ans *= A(i).sum
+    ans %= MOD
+  }
+  println(ans)
 }
