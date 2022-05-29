469c62260ba45c285d1915cb400b3b32568f1be5
Wisteria30 <skymars0327@yahoo.co.jp>
2022-05-29 23:35:39 +0900

    典型90問 082まで

--- src/main/scala/typical90/079/Main.scala
+++ src/main/scala/typical90/079/Main.scala
@@ -1,7 +1,46 @@
 import java.util.Scanner
+import scala.collection.mutable
 
 object Main extends App {
+  private implicit def intIsZero(i: Int): Boolean = if (i == 0) true else false
+
   val sc: Scanner = new Scanner(System.in)
 
-  println("Hello World!")
+  val H, W = sc.nextInt
+  val A = IndexedSeq.fill(H)(IndexedSeq.fill(W)(sc.nextInt))
+  val B = IndexedSeq.fill(H)(IndexedSeq.fill(W)(sc.nextInt))
+  //  X + (A - B) = 0 => X = B - A
+  val BminusA = mutable.IndexedSeq.fill(H)(mutable.IndexedSeq.fill(W)(0))
+
+  for (i <- 0 until H) {
+    for (j <- 0 until W) {
+      BminusA(i)(j) = B(i)(j) - A(i)(j)
+    }
+  }
+  val nx = List(0, 1, 0, 1)
+  val ny = List(0, 0, 1, 1)
+  //  @formatter:off
+  var cnt = 0L
+
+  for (i <- 0 until H - 1) {
+    for (j <- 0 until W - 1) {
+      val diff = -BminusA(i)(j)
+      for (k <- 0 until 4) {
+        val dx = j + nx(k)
+        val dy = i + ny(k)
+        BminusA(dy)(dx) += diff
+      }
+      cnt += math.abs(diff)
+    }
+  }
+
+  //  @formatter:off
+  var ans = BminusA.foldLeft(true)((acc, i) => i.foldLeft(true)((acc2, j) => acc2 & j) & acc)
+
+  if (ans) {
+    println("Yes")
+    println(cnt)
+  } else {
+    println("No")
+  }
 }
--- src/main/scala/typical90/082/Main.scala
+++ src/main/scala/typical90/082/Main.scala
@@ -2,6 +2,23 @@ import java.util.Scanner
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
+  val MOD = 1000000007
 
-  println("Hello World!")
+  val L, R = BigInt(sc.nextLong)
+  val ans = (solve(R) - solve(L - 1)) % MOD
+  println(ans)
+
+
+  def solve(N: BigInt): BigInt = {
+    val pow10Int = (n: Double) => BigInt(math.pow(10, n).toLong)
+    val digit = N.toString.length
+    var ret = BigInt(0)
+    for (i <- 0 until digit - 1) {
+      val arithmetic = (pow10Int(i) + ("9" * (i + 1)).toLong) * 9 * pow10Int(i) / 2
+      ret += (i + 1) * arithmetic
+    }
+    val arithmetic = (pow10Int(digit - 1) + N) * (N - pow10Int(digit - 1) + 1) / 2
+    ret += digit * arithmetic
+    ret
+  }
 }
