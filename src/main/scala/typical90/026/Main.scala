aeb5fb94c3544e61c7ca6678acf82cb22c1e6cc4
Wisteria30 <skymars0327@yahoo.co.jp>
2022-06-02 12:48:14 +0900

    典型90問 026

--- src/main/scala/typical90/026/Main.scala
+++ src/main/scala/typical90/026/Main.scala
@@ -1,7 +1,40 @@
 import java.util.Scanner
+import scala.collection.mutable
 
 object Main extends App {
-  val sc: Scanner = new Scanner(System.in)
+  val sc = new Scanner(System.in)
 
-  println("Hello World!")
+  val N = sc.nextInt
+  val graph = Array.fill(N)(List.empty[Int])
+  for (_ <- 0 until N - 1) {
+    val A, B = sc.nextInt - 1
+    graph(A) = B :: graph(A)
+    graph(B) = A :: graph(B)
+  }
+
+  val queue = mutable.Queue.empty[Int]
+
+  val twoColors = Array.fill(N)(-1)
+  twoColors(0) = 0
+  queue.enqueue(0)
+
+  while (queue.nonEmpty) {
+    val edge = queue.dequeue()
+    val nextColor = 1 - twoColors(edge)
+    for (next <- graph(edge)) {
+      if (twoColors(next) == -1) {
+        twoColors(next) = nextColor
+        queue.enqueue(next)
+      }
+    }
+  }
+
+  val zeros = twoColors.zipWithIndex.filter(_._1 == 0).map(_._2 + 1)
+  val ones = twoColors.zipWithIndex.filter(_._1 == 1).map(_._2 + 1)
+
+  if (zeros.size >= ones.size) {
+    println(zeros.slice(0, N / 2).mkString(" "))
+  } else {
+    println(ones.slice(0, N / 2).mkString(" "))
+  }
 }
