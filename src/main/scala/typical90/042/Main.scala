6b42b5f4e851800df2f268750a3b8672703db1e2
Wisteria30 <skymars0327@yahoo.co.jp>
2022-06-05 22:08:25 +0900

    典型90問 042

--- src/main/scala/typical90/042/Main.scala
+++ src/main/scala/typical90/042/Main.scala
@@ -2,6 +2,22 @@ import java.util.Scanner
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
+  val N = 100000
+  val MOD = 1000000007
 
-  println("Hello World!")
+  val K = sc.nextInt
+  if (!(K % 9 == 0)) {
+    println(0)
+    System.exit(0)
+  }
+
+  val dp = Array.fill(N)(0)
+  dp(0) = 1
+  for (i <- 1 to K) {
+    for (j <- 1 to (i min 9)) {
+      dp(i) += dp(i - j)
+      dp(i) %= MOD
+    }
+  }
+  println(dp(K))
 }
