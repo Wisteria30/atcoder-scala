c665bd37ffe3e602f4131fb8aceb00f657d9cf7e
Wisteria30 <skymars0327@yahoo.co.jp>
2022-06-07 18:16:59 +0900

    典型90問 058

--- src/main/scala/typical90/058/Main.scala
+++ src/main/scala/typical90/058/Main.scala
@@ -2,6 +2,30 @@ import java.util.Scanner
 
 object Main extends App {
   val sc: Scanner = new Scanner(System.in)
+  val MOD = 100000
 
-  println("Hello World!")
+  val N, K = sc.nextLong
+  val visited = Array.fill[Boolean](MOD)(false)
+
+  val cycle = Array.fill(MOD)(-1)
+
+  var x = calcZ(N.toInt)
+  var cnt = 0
+  while (!visited(x)) {
+    visited(x) = true
+    cycle(cnt) = x
+    x = calcZ(x)
+    cnt += 1
+  }
+  val loopStart = cycle.indexOf(calcZ(cycle(cnt- 1)))
+  val loop = cnt - loopStart
+  val nonLoop = cnt - loop
+
+  if (K <= nonLoop) println(cycle((K - 1).toInt))
+  else println(cycle(((K - 1 - nonLoop) % loop + nonLoop).toInt))
+
+  def calcZ(x: Int): Int = {
+    val y = x.toString.toCharArray.map(_ - '0').sum
+    (x + y) % MOD
+  }
 }
